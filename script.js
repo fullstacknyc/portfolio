9c33c7c5ff27acf24b5fa878cfbc5dc435ad4077
fullstacknyc <myemailxd@icloud.com>
2025-08-15 14:58:40 -0400

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("pause")[0].style.display = "none";
+
+document.getElementsByClassName("viewport")[0].innerHTML = `
+    <h1>1v1</h1>
+    <h2>Press Space to Start</h2>
+    <h3>WASD to Move | Mouse to Aim | Left Click to Shoot</h3>
+`;
