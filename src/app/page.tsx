32a43f516d39a6348c7ea1cb41530d07c9387d05
fullstacknyc <myemailxd@icloud.com>
2025-10-05 19:26:53 -0400

    removing projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import ProjectRotator from "./components/ProjectRotator";
 import SkillRotator from "./components/SkillRotator";
 
 import { useState, useEffect } from "react";
@@ -20,21 +19,10 @@ const skillset = [
   "Linux",
 ]
 
-const projectsset = [
-  "File Transfer App",
-  "Gamify Your Tasks",
-  "Weather Dashboard",
-  "Sales Page",
-  "Blog Platform",
-  "Chat Application",
-  "E-commerce Site",
-  "Portfolio Website",
-]
 export default function Home() {
   return (
     <main>
       <SkillRotator skills={skillset} interval={3000} />
-      <ProjectRotator projects={projectsset} interval={3000} />
     </main>
   );
 }
