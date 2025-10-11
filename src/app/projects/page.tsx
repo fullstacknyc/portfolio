c6791e8a0354b420fff4cf8370e647e65b189ca6
fullstacknyc <jgomezval@icloud.com>
2025-10-10 21:21:19 -0400

    mvp working links

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState, useEffect} from "react";
+
+export default function projects() {
+    return <>
+    <html lang="en">
+        <head>
+            <title>Projects</title>
+        </head>
+        <body>
+            <h1>Projects</h1>
+            <p>Dynamic displays of my GitHub repositories.</p>
+        </body>
+    </html></>
+}
